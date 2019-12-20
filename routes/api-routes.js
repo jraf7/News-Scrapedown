111e43280b9927a26c97290061cf2cc601164340
Jonathan Johnson <jraf7@icloud.com>
2019-12-19 22:33:58 -0500

    create routes folder

---
+++ routes/api-routes.js
@@ -0,0 +1,110 @@
+let db = require("../models");
+let cheerio = require("cheerio");
+let axios = require("axios");
+
+module.exports = function (app) {
+
+    app.get("/scrape", function (req, res) {
+        console.log("starting ajax call")
+        axios.get("https://www.theonion.com/search?blogId=1636079510")
+        .then(function (response) {
+            let $ = cheerio.load(response.data);
+            $("h2").each(function (i, element) {
+                let result = {};
+                result.title = $(this).text();
+                result.link = $(this).parent().attr("href");
+                result.image = $(this).parent().parent().parent().prev().children().children().children().children().attr("srcset");
+                
+                console.log(result.image);
+                // db.Article.create(result)
+                //     .then(function (dbArticle) {
+                //         console.log(dbArticle);
+                //     })
+                //     .catch(function (err) {
+                //         console.log(err)
+                //     });                  
+            })
+            console.log("Scrape Completed")
+            res.send("Scrape Complete");
+        })
+    })
+
+    app.get("/api/articles", function (req, res) {
+        db.Article.find({})
+            .populate("comments")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
+    });
+    
+    app.get("/api/articles/:id", function (req, res) {
+        db.Article.find({ _id: req.params.id })
+            .populate("comments")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
+    })
+
+    app.get("/api/articles/saved", function (req, res) {
+        db.Article.find( { saved: true})
+            .populate("comments")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            })
+    });
+
+    app.get("/api/articles/saved/:id", function (req, res) {
+        db.Article.find( { saved: true, _id: req.params.id })
+            .populate("comments")
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function(err) {
+                res.json(err);
+            })
+    });
+
+    app.get("/api/delete/:id", function (req, res) {
+        db.Article.remove({
+                _id: mongojs.ObjectID(req.params.id)
+            },
+            function (error, remove) {
+                if (error) {
+                    res.json(error);
+                    console.log(error);
+                } else {
+                    console.log(remove);
+                    res.json(remove);
+                }
+            }
+        )
+    });
+
+    //Post Routez: post id => comments
+
+    app.post("/api/articles/:id", function (req, res) {
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                return db.Note.findOneAndUpdate(
+                    { _id: req.params.id},
+                    { $push: { note: dbNote._id} },
+                    { new: true}
+                );
+                }
+            )
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+    })
+
+
+}
--- server.js
+++ server.js
@@ -1,114 +1,18 @@
 let express = require("express");
 let mongoose = require("mongoose");
-let axios = require("axios");
-let cheerio = require("cheerio");
-let db = require("./models");
+
 let PORT = 3000;
 let app = express();
-app.use(express.urlencoded({
-    extended: true
-}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 mongoose.connect(MONGODB_URI);
 
-//API Routes
-//Get Routes: all posts, posts id
-
-app.get("/", function (req, res){
-    res.sendFile("./public/index.html");
-})
-
-app.get("/scrape", function (req, res) {
-    axios.get("https://www.theonion.com/").then(function (response) {
-        let $ = cheerio.load(response.data);
-        $("section a").each(function (i, element) {
-            let result = {};
-            result.title = $(this).attr("title");
-            result.link = $(this).attr("href");
-            result.image = $(this).children("img").attr("src");
-
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                        console.log(dbArticle);
-                    })
-                .catch(function (err) {
-                        console.log(err)
-                    });
-        })
-        res.send("Scrape Complete");
-    })
-})
-
-app.get("/articles", function (req, res) {
-    db.Article.find({})
-        .then(function(dbArticle){
-            res.json(dbArticle);
-        })
-        .catch(function(err){
-            res.json(err);
-        })
-});
-
-app.get("/delete/:id", function(req, res){
-    db.Article.remove(
-        {
-            _id: mongojs.ObjectID(req.params.id)
-        },
-        function (error, remove) {
-            if (error) {
-                res.json(error);
-                console.log(error);
-            } else {
-                console.log(remove);
-                res.json(remove);
-            }
-        }
-    )
-});
-
-
-app.put("/saved/:id", function (req, res) {
-    db.Article.update(
-        {
-            _id: mongojs.ObjectID(req.params.id)
-        },
-        {
-            $set: {
-                saved: true
-            }
-        }
-    )
-    .then(function(data){
-        res.json(data);
-    })
-    .catch(function(err){
-        res.json(err)
-    })
-})
-
-app.get("/saved", function (req, res) {
-    db.Article.find({})
-        .then(function(dbArticle){
-            res.json(dbArticle);
-        })
-        .catch(function(err){
-            res.json(err);
-        })
-});
-
-//Post Routez: post id => comments
-app.post("/comment", function (req, res) {
-    db.Note.create(req.body)
-        .then(function(dbNote){
-            res.json(dbNote)
-        }
-        .catch(function(err){
-            res.json(err)
-        })
-    )
-})
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(PORT, function () {
     console.log("============================");
